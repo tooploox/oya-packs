b5f60391f10af4083e302467882134af5560e503
Paweł Bielicki <pabielicki@hotmail.com>
2019-02-07 13:33:21 +0100

    Use oya render.

--- circleci/circleci.sh
+++ circleci/circleci.sh
@@ -1,21 +1,18 @@
-DEFAULT_LANG="ruby"
-lang=$DEFAULT_LANG
 
-detectProjectLang () {
-    if [ -f package.json ]; then
-        lang="nodejs"
-    fi
-    if [ -f Rakefile ]; then
-        lang="ruby"
-    fi
-    if [[ -n $(find . -maxdepth 1 -type f -name "*.go") ]]; then
-        lang="go"
-    fi
-    if [[ -n $(find . -name "__init__.py") ]]; then
-        lang="python"
-    fi
-}
-detectProjectLang
+if [ -f package.json ]; then
+    image=circleci/node:11.9.0-stretch-node-browsers
+fi
+if [ -f Rakefile ]; then
+    image=circleci/ruby:2.5-node-browsers
+fi
+if [[ -n $(find . -maxdepth 1 -type f -name "*.go") ]]; then
+    image=circleci/golang:1.11.5-node-browsers
+fi
+if [[ -n $(find . -name "__init__.py") ]]; then
+    image=circleci/python:3.7.2-node-broesers
+fi
 
 BasePath=$1
-cp $BasePath/config-$lang.yml ./.circleci/config.yml
+oya render -f Oyafile -a circleci $BasePath/config.yml
+mkdir -p .circleci
+mv config.yml .circleci/.
