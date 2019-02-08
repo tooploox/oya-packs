7c4802bf1aae468e6283f45d0cb44c89de902f35
bart <bart.dy@gmail.com>
2019-02-08 22:53:41 +0100

    Misspell

--- docker/overwrite.sh
+++ docker/overwrite.sh
@@ -1,9 +1,7 @@
 if [ -f Dockerfile ]; then
-    read -p "Dockerfile found in $(pwd). Override? [y/N] " yn
+    read -p "Dockerfile found in $(pwd). Overwrite? [y/N] " yn
     if [ "$yn" = "Y" ] || [ "$yn" = "y" ]; then
         echo "Y"
-    else
-        echo "N"
     fi
 else
     echo "Y"
