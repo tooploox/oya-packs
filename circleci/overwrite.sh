95e5826152564cea129457a7cecee66ee16f823f
bart <bart.dy@gmail.com>
2019-07-26 13:13:13 +0200

    circleci fix

--- circleci/overwrite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ -f .circleci/config.yml ]; then
-    read -p "config.yml found in $(pwd)/.cricleci. Overwrite? [y/N] " yn
-    if [ "$yn" = "Y" ] || [ "$yn" = "y" ]; then
-        echo "Y"
-    fi
-else
-    echo "Y"
-fi
